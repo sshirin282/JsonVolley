d2662ec0d677e2b1853d51395b5ff78752727c51
Maxgen cocos <sshirin058@gmail.com>
2021-06-21 11:51:03 +0530

    Json

--- app/src/main/java/com/example/jsonvolley/MainActivity.kt
+++ app/src/main/java/com/example/jsonvolley/MainActivity.kt
@@ -14,13 +14,16 @@ import org.json.JSONObject
 class MainActivity : AppCompatActivity() {
     lateinit var textView: TextView
     lateinit var textView1:TextView
+    lateinit var textView2:TextView
+    lateinit var textView3:TextView
     val url:String="https://api.androidhive.info/volley/person_object.json"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView=findViewById(R.id.text)
         textView1=findViewById(R.id.text1)
-
+        textView2=findViewById(R.id.text2)
+        textView3=findViewById(R.id.text3)
 
         val request:StringRequest= StringRequest(Request.Method.GET,url,Response.Listener {
           response ->
@@ -30,6 +33,14 @@ class MainActivity : AppCompatActivity() {
             textView.text=name
             val email:String=jsonObject.getString("email")
             textView1.text=email
+            val jsonObject1:JSONObject= jsonObject.getJSONObject("phone")
+            val home:String=jsonObject1.getString("home")
+            Log.e("home>>>",jsonObject1.getString("home"))
+           textView2.text=home
+            val mobile:String=jsonObject1.getString("mobile")
+            Log.e("mobile>>>",jsonObject1.getString("mobile"))
+            textView3.text=mobile
+
 
 
 
